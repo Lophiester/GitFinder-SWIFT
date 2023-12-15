562664d9f6fe698b7eb04504539c24146cdf4e99
lophiester <gilcharles2022@hotmail.com>
2023-12-15 16:39:04 +0900

    MockData

--- GitFinder/ContentView.swift
+++ GitFinder/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Circle()
+                .foregroundStyle(.gray)
+                .frame(width: 124,height: 124)
+            Text("Username")
+                .font(.title3)
+                .bold()
+            Text("This is where the github bio will go.")
+            Spacer()
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
+
+
+struct UserData: Codable{
+    let login : String
+    let avatarUrl : String
+    let bio : String
+}
+
