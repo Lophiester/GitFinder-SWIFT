c9326ef09338948ddcf18d5104b0ad4eb2080a09
lophiester <gilcharles2022@hotmail.com>
2023-12-15 16:06:08 +0900

    Initial Commit

---
+++ GitFinder/GitFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFinderApp.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct GitFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
