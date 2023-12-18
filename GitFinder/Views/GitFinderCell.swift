9b6c29abdcf0b6a9c21c13352f055d401c4f21b4
lophiester <gilcharles2022@hotmail.com>
2023-12-18 19:03:32 +0900

    MVVM Design Pattern part 3

---
+++ GitFinder/Views/GitFinderCell.swift
@@ -0,0 +1,18 @@
+//
+//  GitFinderCell.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/18.
+//
+
+import SwiftUI
+
+struct GitFinderCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitFinderCell()
+}
