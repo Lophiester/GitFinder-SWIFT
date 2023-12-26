bfc3cc70459cac20104ef8456eafba03810aea6a
lophiester <gilcharles2022@hotmail.com>
2023-12-26 18:21:25 +0900

    UserNotFoundView and FindUserView

---
+++ GitFinder/Views/FindUserView.swift
@@ -0,0 +1,18 @@
+//
+//  findUserView.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/26.
+//
+
+import SwiftUI
+
+struct findUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    findUserView()
+}
