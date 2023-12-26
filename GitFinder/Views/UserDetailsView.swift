4745b294e654eeaea9f2a73628365ae370476040
lophiester <gilcharles2022@hotmail.com>
2023-12-26 19:00:50 +0900

    Refactored code

---
+++ GitFinder/Views/UserDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailsView.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/26.
+//
+
+import SwiftUI
+
+struct UserDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailsView()
+}
