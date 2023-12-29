2e815674058edc7ebc6b1538aa98abf7333d080b
lophiester <gilcharles2022@hotmail.com>
2023-12-29 15:55:44 +0900

    SearchableView and viewModel

---
+++ GitFinder/Views/SearchableView/SearchableView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchableView.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/29.
+//
+
+import SwiftUI
+
+struct SearchableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchableView()
+}
