6e99eed43e48f8aaebc064d9e1c78244141496a6
lophiester <gilcharles2022@hotmail.com>
2023-12-29 14:33:51 +0900

    WebKit

---
+++ GitFinder/Model/WebKit.swift
@@ -0,0 +1,8 @@
+//
+//  WebKit.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/29.
+//
+
+import Foundation
---
+++ GitFinder/Views/WebkitView.swift
@@ -0,0 +1,18 @@
+//
+//  WebkitView.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2023/12/29.
+//
+
+import SwiftUI
+
+struct WebkitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebkitView()
+}
