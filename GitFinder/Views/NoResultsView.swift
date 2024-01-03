14ba843d23d87846285f0f2d472f0dac7037a469
lophiester <gilcharles2022@hotmail.com>
2024-01-03 18:14:23 +0900

    In the NoResultView case, the searchable couldn't find someone.

---
+++ GitFinder/Views/NoResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoResultsView.swift
+//  GitFinder
+//
+//  Created by Charles Yamamoto on 2024/01/03.
+//
+
+import SwiftUI
+
+struct NoResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoResultsView()
+}
